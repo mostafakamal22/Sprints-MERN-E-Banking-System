3542192e1a4d7610034e832e360aaf298f185d0a
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-06-13 16:44:16 +0200

    install tailwind package, add tailwind configs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     fetch("http://localhost:5000/api/users", {
       method: "get",
@@ -15,41 +11,7 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hey every one</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <div className="text-center text-red-900">Home Pgae</div>;
 }
 
 export default App;
