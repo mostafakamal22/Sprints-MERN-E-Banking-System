a7200c19a322de0480f91f27fb51da38f86103c6
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-07-25 19:45:00 +0200

    edit account details component with new styles colors

--- Frontend/src/components/account/AccountDetails.jsx
+++ Frontend/src/components/account/AccountDetails.jsx
@@ -4,7 +4,7 @@ import { FcRating } from "react-icons/fc";
 
 export const AccountDetails = ({ account }) => {
   return (
-    <div className="flex items-center justify-center flex-col gap-4 px-6 py-8 my-10 bg-blue-200 border-y-4 border-blue-800 rounded shadow select-none">
+    <div className="flex items-center justify-center flex-col gap-4 px-6 py-8 my-10 bg-blue-200 border-y-4 border-blue-800 rounded shadow">
       <h3 className="w-full flex items-center text-xl my-5 p-3 text-left font-bold   text-blue-900 bg-slate-50 rounded shadow-md">
         <FcRating className="mr-1" size={35} />
         Account Details:-
@@ -14,7 +14,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Account ID
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account._id}
         </span>
       </div>
@@ -23,7 +23,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Created At
         </p>
-        <span className="w-full lg:w-auto bg-violet-900 px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white px-4 py-2 rounded-md">
           {moment(account.createdAt).format("d MMMM YYYY")}
         </span>
       </div>
@@ -32,7 +32,7 @@ export const AccountDetails = ({ account }) => {
         <p className=" w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Balance
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white px-4 py-2 rounded-md">
           {new Intl.NumberFormat("ar-EG", {
             style: "currency",
             currency: "EGP",
@@ -44,7 +44,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           OutGoing Transcations
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.out.length > 0
             ? new Intl.NumberFormat("ar-EG", {
                 style: "currency",
@@ -66,7 +66,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Incoming Transcations
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.in.length > 0
             ? new Intl.NumberFormat("ar-EG", {
                 style: "currency",
@@ -88,7 +88,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Deposit Amount
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.deposit_logs.length > 0
             ? new Intl.NumberFormat("ar-EG", {
                 style: "currency",
@@ -110,7 +110,7 @@ export const AccountDetails = ({ account }) => {
         <p className="w-full lg:w-auto bg-slate-900  px-4 py-2 rounded-md">
           Withdrawal Amount
         </p>
-        <span className="w-full lg:w-auto bg-violet-900  px-4 py-2 rounded-md">
+        <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.withdraw_logs.length > 0
             ? new Intl.NumberFormat("ar-EG", {
                 style: "currency",
