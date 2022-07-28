9e8e237ddab1a2818afc037dab045ff2fbb6baf0
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-07-29 00:29:25 +0200

    prepare server for production build

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -31,6 +31,17 @@ app.use("/api/account", accountRoute);
 //account requests Router
 const accountRequestRoute = require("./routes/accountRequestRoutes");
 app.use("/api/request", accountRequestRoute);
+
+//serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "Frontend", "build", "index.html")
+    )
+  );
+}
 
 app.listen(process.env.PORT, () => {
   console.log("server is running");
