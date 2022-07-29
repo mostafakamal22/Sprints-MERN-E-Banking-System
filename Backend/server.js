8c9d717eed80b5ca4bdf59eb59a3c7e00eeea7c5
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-07-29 21:09:01 +0200

    update server static serve frontend folder path

--- Backend/server.js
+++ Backend/server.js
@@ -34,12 +34,10 @@ app.use("/api/request", accountRequestRoute);
 
 //serve Frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/build")));
+  app.use(express.static("Frontend/build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "Frontend", "build", "index.html")
-    )
+    res.sendFile(path.join(__dirname, "/Frontend/build/index.html"))
   );
 } else {
   app.get("*", (req, res) => res.send("hi no production"));
