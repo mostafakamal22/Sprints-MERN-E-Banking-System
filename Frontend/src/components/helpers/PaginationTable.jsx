c535ffa627ace72f2bcb55c6ff1ff0b3968cfe8d
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-03-27 19:38:54 +0200

    fix table styles

--- Frontend/src/components/helpers/PaginationTable.jsx
+++ Frontend/src/components/helpers/PaginationTable.jsx
@@ -12,12 +12,14 @@ export const PaginationTable = ({
   const [page, setPage] = useState(1);
   const { slice, range } = useTable(tableBodyData, page, rowsPerPage);
   return (
-    <div className="overflow-x-auto relative shadow-md sm:rounded-lg my-10 border-y-4 border-blue-800 rounded">
-      <table className="w-full text-sm font-bold text-gray-500 ">
-        <thead className="text-gray-900 uppercase bg-gray-300">
+    <div className="overflow-x-auto relative shadow-md sm:rounded-lg my-10 border-y-4 border-red-800 rounded scrollbar-thin scrollbar-track-transparent scrollbar-thumb-blue-400">
+      <table className="w-full !text-xs text-gray-500 ">
+        <thead className="text-gray-900 uppercase bg-gray-300 !font-semibold">
           {tableHeader}
         </thead>
-        <tbody>{slice.map((item, index) => tableRow(item, index))}</tbody>
+        <tbody className="!font-normal">
+          {slice.map((item, index) => tableRow(item, index))}
+        </tbody>
       </table>
 
       <Pagination range={range} slice={slice} setPage={setPage} page={page} />
