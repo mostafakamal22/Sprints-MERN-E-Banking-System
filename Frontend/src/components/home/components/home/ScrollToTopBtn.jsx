94e85351076e24175f363b41eceba19875c75f1c
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-02 02:26:48 +0200

    edit scrollToTopButton background color from gradient  to slate

--- Frontend/src/components/home/components/home/ScrollToTopBtn.jsx
+++ Frontend/src/components/home/components/home/ScrollToTopBtn.jsx
@@ -6,7 +6,7 @@ export const ScrollToTopBtn = ({ startAnimation }) => {
     <a
       title="Scroll To Top"
       href="#"
-      className={`block fixed right-4 bottom-4 md:right-8 md:bottom-8 z-20 w-10 h-10  md:h-12 md:w-12 curser-pointer bg-gradient-to-b from-indigo-500   to-pink-500 flex justify-center items-center rounded-full shadow-lg animate-bounce transition-all duration-300 ease-in-out ${
+      className={`fixed right-4 bottom-4 md:right-8 md:bottom-8 z-20 w-10 h-10  md:h-12 md:w-12 curser-pointer bg-slate-300 flex justify-center items-center rounded-full shadow-2xl animate-bounce transition-all duration-300 ease-in-out ${
         startAnimation ? "opacity-100" : "opacity-0 bottom-full md:bottom-full"
       }`}
     >
