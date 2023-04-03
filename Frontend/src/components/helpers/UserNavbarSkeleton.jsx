3ab42ae368a0850f7d7b31702893cbf84ed5bd52
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-03 20:19:49 +0200

    edit user navbar skeleton styles

--- Frontend/src/components/helpers/UserNavbarSkeleton.jsx
+++ Frontend/src/components/helpers/UserNavbarSkeleton.jsx
@@ -1,17 +1,8 @@
 export const UserNavbarSkeleton = () => {
   const Skeleton = new Array(15).fill().map((_, i) => (
-    <li
-      key={i}
-      className={`flex w-full justify-between items-center ${
-        i === 0 ? "mb-12" : "mb-6"
-      }`}
-    >
-      {i === 0 ? (
-        <span className="w-full p-5 rounded shadow bg-blue-200 animate-pulse"></span>
-      ) : (
-        <span className="w-full flex items-center p-2 rounded shadow bg-blue-200 animate-pulse"></span>
-      )}
-    </li>
+    <span key={i} className="w-full flex justify-center items-center mb-6">
+      <span className="w-full p-4 rounded shadow bg-blue-200 animate-pulse"></span>
+    </span>
   ));
 
   return Skeleton;
